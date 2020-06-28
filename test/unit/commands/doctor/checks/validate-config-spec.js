11dd0234af70aed3c33aa2f58df0d2de770198fc
Austin Burdine <acburdine@gmail.com>
2020-06-28 09:50:43 -0400

    refactor(doctor): refactor config check

--- test/unit/commands/doctor/checks/validate-config-spec.js
+++ test/unit/commands/doctor/checks/validate-config-spec.js
@@ -1,8 +1,7 @@
-'use strict';
-const expect = require('chai').expect;
+const {expect} = require('chai');
 const sinon = require('sinon');
 
-const errors = require('../../../../../lib/errors');
+const {ConfigError} = require('../../../../../lib/errors');
 const {setupTestFolder, cleanupTestFolders} = require('../../../../utils/test-folder');
 const advancedOpts = require('../../../../../lib/tasks/configure/options');
 
@@ -18,99 +17,98 @@ describe('Unit: Doctor Checks > validateConfig', function () {
         cleanupTestFolders();
     });
 
-    it('skips if instance not set', function () {
+    it('skips if instance not set', async function () {
         const skipStub = sinon.stub();
 
-        validateConfig({instance: null}, {skip: skipStub});
+        await validateConfig({instance: null}, {skip: skipStub});
         expect(skipStub.calledOnce).to.be.true;
     });
 
-    it('skips check, when instance is currently running', function () {
+    it('skips check, when instance is currently running', async function () {
         const runningStub = sinon.stub().resolves(true);
         const skipStub = sinon.stub();
 
-        return validateConfig({instance: {isRunning: runningStub}}, {skip: skipStub}).then(() => {
-            expect(runningStub.calledOnce).to.be.true;
-            expect(skipStub.calledOnce).to.be.true;
-        });
+        await validateConfig({instance: {isRunning: runningStub}}, {skip: skipStub});
+        expect(runningStub.calledOnce).to.be.true;
+        expect(skipStub.calledOnce).to.be.true;
     });
 
-    it('rejects if environment is passed and no config exists for that environment', function () {
+    it('rejects if environment is passed and no config exists for that environment', async function () {
         const env = setupTestFolder();
-        const cwdStub = sinon.stub(process, 'cwd').returns(env.dir);
         const runningStub = sinon.stub().resolves(false);
 
-        return validateConfig({
-            system: {environment: 'testing'},
-            instance: {isRunning: runningStub}
-        }).then(() => {
+        try {
+            await validateConfig({
+                system: {environment: 'testing'},
+                instance: {isRunning: runningStub, dir: env.dir}
+            });
+
             expect(false, 'error should have been thrown').to.be.true;
-        }).catch((error) => {
-            expect(error).to.be.an.instanceof(errors.ConfigError);
+        } catch (error) {
+            expect(error).to.be.an.instanceof(ConfigError);
             expect(error.message).to.match(/Config file is not valid JSON/);
             expect(error.options.environment).to.equal('testing');
-            expect(cwdStub.calledOnce).to.be.true;
             expect(runningStub.calledOnce).to.be.true;
-        });
+        }
     });
 
-    it('rejects if environment is passed and the config file is not valid json', function () {
+    it('rejects if environment is passed and the config file is not valid json', async function () {
         const env = setupTestFolder({files: [{path: 'config.testing.json', contents: 'not json'}]});
-        const cwdStub = sinon.stub(process, 'cwd').returns(env.dir);
         const runningStub = sinon.stub().resolves(false);
 
-        return validateConfig({
-            system: {environment: 'testing'},
-            instance: {isRunning: runningStub}
-        }).then(() => {
+        try {
+            await validateConfig({
+                system: {environment: 'testing'},
+                instance: {isRunning: runningStub, dir: env.dir}
+            });
             expect(false, 'error should have been thrown').to.be.true;
-        }).catch((error) => {
-            expect(error).to.be.an.instanceof(errors.ConfigError);
+        } catch (error) {
+            expect(error).to.be.an.instanceof(ConfigError);
             expect(error.message).to.match(/Config file is not valid JSON/);
             expect(error.options.environment).to.equal('testing');
-            expect(cwdStub.calledOnce).to.be.true;
             expect(runningStub.calledOnce).to.be.true;
-        });
+        }
     });
 
-    it('rejects with error if config values does not pass', function () {
+    it('rejects with error if config values does not pass', async function () {
         const config = {server: {port: 2368}};
         const env = setupTestFolder({files: [{path: 'config.testing.json', content: config, json: true}]});
         const urlStub = sinon.stub(advancedOpts.url, 'validate').returns('Invalid URL');
         const portStub = sinon.stub(advancedOpts.port, 'validate').returns('Port is in use');
-        sinon.stub(process, 'cwd').returns(env.dir);
         const runningStub = sinon.stub().resolves(false);
 
-        return validateConfig({
-            system: {environment: 'testing'},
-            instance: {isRunning: runningStub}
-        }).then(() => {
+        try {
+            await validateConfig({
+                system: {environment: 'testing'},
+                instance: {isRunning: runningStub, dir: env.dir}
+            });
             expect(false, 'error should have been thrown').to.be.true;
-        }).catch((error) => {
-            expect(error).to.be.an.instanceof(errors.ConfigError);
+        } catch (error) {
+            expect(error).to.be.an.instanceof(ConfigError);
             expect(error.message).to.equal('Port is in use');
             expect(error.options.config).to.deep.equal({'server.port': 2368});
             expect(urlStub.called).to.be.false;
             expect(portStub.calledOnce).to.be.true;
             expect(portStub.calledWithExactly(2368)).to.be.true;
-        });
+        }
     });
 
-    it('passes if all validate functions return true', function () {
+    it('passes if all validate functions return true', async function () {
         const config = {server: {port: 2368}};
         const env = setupTestFolder({files: [{path: 'config.testing.json', content: config, json: true}]});
         const urlStub = sinon.stub(advancedOpts.url, 'validate').returns(true);
         const portStub = sinon.stub(advancedOpts.port, 'validate').returns(true);
-        sinon.stub(process, 'cwd').returns(env.dir);
         const runningStub = sinon.stub().resolves(false);
 
-        return validateConfig({
-            system: {environment: 'testing'},
-            instance: {isRunning: runningStub}
-        }).then(() => {
+        try {
+            await validateConfig({
+                system: {environment: 'testing'},
+                instance: {isRunning: runningStub, dir: env.dir}
+            });
+        } catch (error) {
             expect(urlStub.called).to.be.false;
             expect(portStub.calledOnce).to.be.true;
             expect(portStub.calledWithExactly(2368)).to.be.true;
-        });
+        }
     });
 });
